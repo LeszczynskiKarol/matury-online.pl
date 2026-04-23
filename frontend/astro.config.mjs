151d601d41384d8459a552f187ddc61c4e079227
LeszczynskiKarol <leszczu123@wp.pl>
2026-04-23 10:05:02 +0200

    noindexy

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -8,7 +8,20 @@ export default defineConfig({
   site: "https://www.matury-online.pl",
   output: "server",
   adapter: node({ mode: "standalone" }),
-  integrations: [react(), tailwind(), sitemap()],
+  integrations: [
+    react(),
+    tailwind(),
+    sitemap({
+      filter: (page) =>
+        !page.includes("/admin") &&
+        !page.includes("/auth/") &&
+        !page.includes("/dashboard") &&
+        !page.includes("/polityka-prywatnosci") &&
+        !page.includes("/privacy-policy") &&
+        !page.includes("/regulamin") &&
+        !page.includes("/terms-of-service"),
+    }),
+  ],
   vite: {
     ssr: { noExternal: ["nanostores", "@nanostores/react"] },
     server: {
